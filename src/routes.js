448312c722af4d9999c4ee1dca02dd7d4b3d385e
rogeriobarreto27 <rogeriooo.silva@gmail.com>
2024-04-16 10:11:32 -0300

    my first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
+
+
+function Routes(){
+    return (
+        <Router>
+            <Switch>
+                <Route/>
+                <Route/>
+            </Switch>
+        </Router>
+    )
+}
